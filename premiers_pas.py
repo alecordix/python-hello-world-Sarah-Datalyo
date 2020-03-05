a8be5574a5f3bded35b506d9ac3cd22ed526c187
Sarah KEZIRE-LARCHER <skezire-larcher.externe@m6.fr>
2020-03-06 00:13:16 +0100

    premier pas en Python

---
+++ premiers_pas.py
@@ -0,0 +1,19 @@
+# On affecte nos variables
+unString = "string"  # une chaîne de caractères
+unInteger = 2  # un entier
+unFloat = 3.0  # un décimal
+unBoolean = True  # un booléen
+
+# On affiche nos variables
+print(unString)
+print(unInteger)
+print(unFloat)
+print(unBoolean)
+
+
+# On peut connaître le type de nos variables à l'aide
+# de la fonction type(maVariable)
+print(type(unString))
+print(type(unInteger))
+print(type(unFloat))
+print(type(unBoolean))
